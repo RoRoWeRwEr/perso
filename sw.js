1f6e9b676692834dc9063ce0e67b2278fe151ac5
RoRoWeRwEr <rorowerwer@gmail.com>
2026-07-18 20:52:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rayanthings-v4';
+const CACHE_NAME = 'rayanthings-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e=>{
